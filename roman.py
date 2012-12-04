5e5ec22ae42a997d26359d289cccfc79b6ca4878
Scott Sadler <github@mashi.org>
2012-12-04 21:08:40 +0000

    completed

--- roman.py
+++ roman.py
@@ -30,3 +30,42 @@ def prep_numeral(numeral):
 
 def sort_numeral(numeral):
     return sorted(numeral, key=rank, reverse=True)
+
+
+def compress_numeral(numeral):
+    numeral = ''.join(numeral)
+    while True:
+        l = len(numeral)
+
+        numeral = numeral.replace('CCCCC', 'D')
+        numeral = numeral.replace('XXXXX', 'L')
+        numeral = numeral.replace('IIIII', 'V')
+
+        numeral = numeral.replace('CCCC', 'CD')
+        numeral = numeral.replace('IIII', 'IV')
+        numeral = numeral.replace('XXXX', 'XL')
+
+        numeral = numeral.replace('DD', 'M')
+        numeral = numeral.replace('LL', 'C')
+        numeral = numeral.replace('VV', 'X')
+
+        if len(numeral) == l:
+            return numeral
+
+
+
+def add_numeral(n1, n2):
+    n1 = prep_numeral(n1)
+    n2 = prep_numeral(n2)
+
+    combined = n1 + n2
+    sorted_ = sort_numeral(combined)
+    out = compress_numeral(sorted_)
+    return out
+
+
+if __name__ == '__main__':
+    import sys
+    n1, n2 = sys.argv[1:]
+    print 'in:', n1, n2
+    print 'out', add_numeral(n1, n2)
